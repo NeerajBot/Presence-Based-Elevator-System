35df77c156848c307cce337b7cb7ab1c3644336e
Edwin Jose <edwinjose900@gmail.com>
2017-12-18 20:09:14 +0530

    new arduino file editted

--- self_sense/self_sense.ino
+++ self_sense/self_sense.ino
@@ -1,7 +1,10 @@
 // #defines will be used to store certain analog values
+# define sensor1 pin_number
+#define sensor2  pin_number
 void setup() 
 {
-  
+  pinMode(sensor1,INPUT);
+  pinMode(senssor2,INPUT);
 
 
 }
