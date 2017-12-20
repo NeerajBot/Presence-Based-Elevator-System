f76599505ed7721fcf24afbba2ec158caabe460f
EDWIN JOSE <EJ@localhost.localdomain>
2017-12-20 12:22:55 +0530

    new files added

--- self_sense/self_sense.ino
+++ self_sense/self_sense.ino
@@ -1,12 +1,20 @@
 // #defines will be used to store certain analog values
-# define sensor1 pin_number
+#define sensor1 pin_number
 #define sensor2  pin_number
+#define thresh_exp 200
+//variable description
+//
+int val;
+int sens1;
+int sens2;
+int th1;
 void setup() 
 {
-  pinMode(sensor1,INPUT);
-  pinMode(senssor2,INPUT);
-
-
+	pinMode(sensor1,INPUT);
+	pinMode(sensor2,INPUT);
+	sens1=analogRead(sensor1);
+	sens2=analogRead(sensor2); 
+			
 }
 
 void loop() 
